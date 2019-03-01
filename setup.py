cc37b313bc5505c7d026a0c6d3a575d716735d7c
Stephanie Singer <sfsinger@campaignscientific.com>
2019-02-28 19:50:58 -0800

    create package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+setup(name='cast_vote_record_conversion',
+      version='0.1',
+      description='Converts cast vote record files to and from various formats',
+      author='Stephanie Singer',
+      author_email='sfsinger@campaignscientific.com',
+      url='https://github.com/sfsinger19103/Cast_Vote_Record_conversion'
+      py_modules=['cast_vote_record_conversion'],
+      )
