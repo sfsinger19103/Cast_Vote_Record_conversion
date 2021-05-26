eacfe5eafdd6b4890bb35b1828ea644e3104ff7a
sfsinger19103 <14353822+sfsinger19103@users.noreply.github.com>
2021-05-26 09:15:34 -0700

    bug fix

--- src/batch_totals/__init__.py
+++ src/batch_totals/__init__.py
@@ -8,6 +8,8 @@ def cvrs_to_batch_totals(
         batches: Path,
         contest: str,
         out_path: Optional[Path],
+        left_join_column: str,
+        right_join_column: str,
 ) -> (pd.DataFrame, List[str]):
     """<votes> is a path to an ES&S Cast Vote Records Export (as of 2021)
     <batches> is a path to an ES&S CVR ID to Tabluator CVR Table file [sic]
@@ -28,18 +30,27 @@ def cvrs_to_batch_totals(
         return df, err
     else:
         try:
-            df_votes = pd.read_excel(votes, index_col="Cast Vote Record")
+            df_votes = pd.read_excel(votes)
+            df_votes[left_join_column] = pd.to_numeric(
+                df_votes[left_join_column], errors="coerce"
+            ).fillna(0).astype("int64")
         except Exception as exc:
             err.append(f"Exception while reading {votes}:\n{exc}")
         try:
-            df_batches = pd.read_excel(batches, index_col="Cast Vote Record", skiprows=5)
+            df_batches = pd.read_excel(batches, skiprows=5)
+            df_batches[right_join_column] = pd.to_numeric(
+                df_batches[right_join_column], errors="coerce"
+            ).fillna(0).astype("int64")
+
         except Exception as exc:
             err.append(f"Exception while reading {batches}:\n{exc}")
     if err:
         return df, err
 
     try:
-        df_all = df_votes.merge(df_batches, how="left", suffixes=["_votes", ""])
+        df_all = df_votes.merge(
+            df_batches, how="left", left_on=left_join_column, right_on=right_join_column, suffixes=["_votes", ""]
+        )
         df = df_all.groupby(["Batch", contest]).size().reset_index().pivot(index="Batch", columns=contest, values=0)
     except Exception as exc:
         err.append(f"Error during data manipulation: {exc}")
@@ -49,9 +60,10 @@ def cvrs_to_batch_totals(
     return df, err
 
 if __name__ == "__main__":
-    v = "/Users/singer3/Documents/VerifiedVoting/ES&S files/Cast Vote Records Export.xlsx"
-    b = "/Users/singer3/Documents/VerifiedVoting/ES&S files/CVR ID to Tabluator CVR Table.xlsx"
+    v = "/Users/singer3/PycharmProjects/Cast_Vote_Record_conversion/samples/ess_2021/Cast Vote Records Export.xlsx"
+    b = "/Users/singer3/PycharmProjects/Cast_Vote_Record_conversion/samples/ess_2021/CVR ID to Tabluator CVR Table.xlsx"
     c = "FAVORITE DOG BREED (18)"
     out = f"/Users/singer3/Documents/VerifiedVoting/ES&S files/{c.replace(' ', '-')}.csv"
-    d_frame, e = cvrs_to_batch_totals(v, b, c, out)
+    join_col = "Cast Vote Record"
+    d_frame, e = cvrs_to_batch_totals(v, b, c, out, join_col, join_col)
     exit()
